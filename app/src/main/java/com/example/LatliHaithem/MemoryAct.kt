c0cac8f837158022cb2279df050636a87bb49e22
Latli-Haytem <85201574+Latli-Haytem@users.noreply.github.com>
2022-04-13 00:29:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/LatliHaithem/MemoryAct.kt
@@ -0,0 +1,12 @@
+package com.example.LatliHaithem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kidz.R
+
+class MemoryAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memory)
+    }
+}
